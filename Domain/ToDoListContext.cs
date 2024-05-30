7891215c3f42c78694c81f846df10af49be642a5
Grant Logsdon <grantlog36@gmail.com>
2024-05-29 22:30:22 -0500

    Renmaed DA folder

---
+++ Domain/ToDoListContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ToDoListContext : DbContext
+    {
+        public ToDoListContext(DbContextOptions<ToDoListContext> options) : base(options) { }
+
+        public DbSet<ListHeader> Lists { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
